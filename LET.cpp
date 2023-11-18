200b4d022267a7ac0dc42504aeeb9b53f25eb77b
Janitha Senani Karunarathna <138304665+CryoWarlocK@users.noreply.github.com>
2023-11-18 20:25:44 +0530

    1. Added more items to menu 2. added new method to containing all the dummy equipments

--- LET.cpp
+++ LET.cpp
@@ -10,9 +10,13 @@
 void runProgram() {
     int Opt;
     cout << "_____________Lab Equipment Tracker Version 1.0______________" << endl;
-    cout << "Enter a option" << endl;
-    cout << "1.Lend Equipment" << endl;
-    cout << "2.Add Equipment " << endl;
+    cout << "Please enter a menu number corresponding to your need." << endl;
+    cout << "1. Lend Equipment" << endl;
+    cout << "2. Add Equipment " << endl;
+    cout << "3. See all Categories " << endl;
+    cout << "4. See all the equipment (sorted) " << endl;
+    cout << "5. Delete an Equipment " << endl;
+    cout << "6. Search Equipment " << endl;
     cout << "______________________________________________________" << endl;
     cin >> Opt;
     switch (Opt) {
@@ -28,29 +32,12 @@ void runProgram() {
     }
 }
 
+
+
 int main() {
     runProgram();
-   /* EquipmentList dll;
-    dll.insertFirst("Oscilloscope", "Measurement", "Tektronix TDS210", "OS-12345", true);
-    dll.insertFirst("Function Generator", "Signal Generator", "Agilent 33500B", "FG-67890", false);
-    dll.insertFirst("Multimeter", "Measurement", "Fluke 87V", "MM-24680", true);
-    dll.insertFirst("Power Supply", "Power", "Agilent E3631A", "PS-13579", false);
-    dll.insertFirst("Spectrum Analyzer", "Signal Analyzer", "Rohde & Schwarz FSW", "SA-54321", true);
-    dll.insertFirst("Digital Logic Analyzer", "Measurement", "Keysight MSO-X 3034T", "DL-98765", false);
-    dll.insertFirst("Signal Generator", "Signal Generator", "Keysight 33522A", "SG-11223", true);
-    dll.insertFirst("Function Generator", "Signal Generator", "Rigol DG1022Z", "FG-45678", false);
-    dll.insertFirst("Digital Oscilloscope", "Measurement", "Tektronix MSO2014B", "OS-13579", true);
-    dll.insertFirst("Voltage Regulator", "Power", "LM317", "VR-98765", false);
-    dll.insertFirst("Signal Generator", "Signal Generator", "B&K Precision 4040A", "SG-24680", true);
-    dll.insertFirst("Logic Probe", "Measurement", "HP 1043A", "LP-11223", false);
-    dll.insertFirst("Power Supply", "Power", "Agilent E3646A", "PS-54321", true);
-    dll.insertFirst("Waveform Generator", "Signal Generator", "Teledyne LeCroy ArbStudio 1102", "WG-13579", false);
-    dll.insertFirst("Frequency Counter", "Measurement", "Fluke 725", "FC-98765", true);
-    dll.insertFirst("DC Motor", "Motors", "NEMA 23", "DM-11223", false);
-    dll.insertFirst("Soldering Station", "Tools", "Hakko FX-888D", "SS-24680", true);
-    dll.insertFirst("Logic Analyzer", "Measurement", "Agilent 16802A", "LA-13579", false);
-    dll.insertFirst("Capacitance Meter", "Measurement", "Keysight U1733C", "CM-98765", true);
-    dll.insertFirst("Digital Power Supply", "Power", "Rigol DP832A", "DPS-11223", false);
+    //dummyData();
+   /* 
     dll.print();
     */
     return 45;
