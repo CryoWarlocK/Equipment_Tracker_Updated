ad6c5b0889845dafc4574357705544f9b25266a4
Janitha Senani Karunarathna <138304665+CryoWarlocK@users.noreply.github.com>
2023-11-18 19:34:21 +0530

    Equipment_Tracker_Updated modified

--- Equipment_Tracker_Updated.cpp
+++ Equipment_Tracker_Updated.cpp
@@ -8,7 +8,7 @@ int main()
     std::cout << "Hello World!\n";
 }
 
-//a test comment
+//a test comment in the branch kitty 
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
