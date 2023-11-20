090f5799fbc1bd22bb42281a436e733581920aab
Janitha Senani Karunarathna <138304665+CryoWarlocK@users.noreply.github.com>
2023-11-21 01:25:19 +0530

    several modifications, but there is an error in line 54

--- Equipment.h
+++ Equipment.h
@@ -97,29 +97,47 @@ public:
         cout << endl;
     }
 
+    // Method to filter and display equipment based on a specific category
+    void filterByCategory(const string& filterCategory) {
+        LabEquipment* current = head;
+        while (current != NULL) {
+            if (current->category == filterCategory) {
+                cout << "Name          : " << current->name << endl;
+                cout << "Category      : " << current->category << endl;
+                cout << "Model         : " << current->model << endl;
+                cout << "Serial Number : " << current->serial << endl;
+                cout << "Availability  : " << current->isLent << endl;
+                cout << endl;
+                cout << endl;
+            }
+            current = current->next;
+        }
+        cout << endl;
+    }
+
+    // Method to traverse the list and print all unique categories
+    void printUniqueCategories() {
+        LabEquipment* current = head;
+        LabEquipment* temp;
+        while (current != NULL) {
+            temp = head;
+            bool isUnique = true;
+            while (temp != current) {
+                if (temp->category == current->category) {
+                    isUnique = false;
+                    break;
+                }
+                temp = temp->next;
+            }
+
+            if (isUnique) {
+                cout << current->category << endl;
+            }
+
+            current = current->next;
+        }
+
+        cout << endl;
+    }
+
 };
-//dummy data 
-void dummyData() {
-    EquipmentList dll;
-    dll.insertFirst("Oscilloscope", "Measurement", "Tektronix TDS210", "OS-12345", true);
-    dll.insertFirst("Function Generator", "Signal Generator", "Agilent 33500B", "FG-67890", false);
-    dll.insertFirst("Multimeter", "Measurement", "Fluke 87V", "MM-24680", true);
-    dll.insertFirst("Power Supply", "Power", "Agilent E3631A", "PS-13579", false);
-    dll.insertFirst("Spectrum Analyzer", "Signal Analyzer", "Rohde & Schwarz FSW", "SA-54321", true);
-    dll.insertFirst("Digital Logic Analyzer", "Measurement", "Keysight MSO-X 3034T", "DL-98765", false);
-    dll.insertFirst("Signal Generator", "Signal Generator", "Keysight 33522A", "SG-11223", true);
-    dll.insertFirst("Function Generator", "Signal Generator", "Rigol DG1022Z", "FG-45678", false);
-    dll.insertFirst("Digital Oscilloscope", "Measurement", "Tektronix MSO2014B", "OS-13579", true);
-    dll.insertFirst("Voltage Regulator", "Power", "LM317", "VR-98765", false);
-    dll.insertFirst("Signal Generator", "Signal Generator", "B&K Precision 4040A", "SG-24680", true);
-    dll.insertFirst("Logic Probe", "Measurement", "HP 1043A", "LP-11223", false);
-    dll.insertFirst("Power Supply", "Power", "Agilent E3646A", "PS-54321", true);
-    dll.insertFirst("Waveform Generator", "Signal Generator", "Teledyne LeCroy ArbStudio 1102", "WG-13579", false);
-    dll.insertFirst("Frequency Counter", "Measurement", "Fluke 725", "FC-98765", true);
-    dll.insertFirst("DC Motor", "Motors", "NEMA 23", "DM-11223", false);
-    dll.insertFirst("Soldering Station", "Tools", "Hakko FX-888D", "SS-24680", true);
-    dll.insertFirst("Logic Analyzer", "Measurement", "Agilent 16802A", "LA-13579", false);
-    dll.insertFirst("Capacitance Meter", "Measurement", "Keysight U1733C", "CM-98765", true);
-    dll.insertFirst("Digital Power Supply", "Power", "Rigol DP832A", "DPS-11223", false);
-    dll.print();
-}
--- LET.cpp
+++ LET.cpp
@@ -8,44 +8,88 @@
 
 
 void runProgram() {
-    int Opt;
-    cout << "_____________Lab Equipment Tracker Version 1.0______________" << endl;
-    cout << "Please enter a menu number corresponding to your need." << endl;
-    cout << "1. Lend Equipment" << endl;
-    cout << "2. Add Equipment " << endl;
-    cout << "3. See all Categories " << endl;
-    cout << "4. See all the equipment (sorted) " << endl;
-    cout << "5. Delete an Equipment " << endl;
-    cout << "6. Search Equipment " << endl;
-    cout << "7. Sample Data Display " << endl;
-    cout << "______________________________________________________" << endl;
-
-    cin >> Opt;
-    switch (Opt) {
-    case 1:     //for lend equipments
-        cout << "Lend Equipment option selected" << endl;
-        break;
-    case 2:
-        cout << "Add Equipment option selected" << endl;
-        break;
-    case 7:
-        cout << "Sample data display option selected" << endl;
-        dummyData();
-        break;
-    default:
-        cout << "Please enter a valid option key" << endl;
-        break;
-    }
+    
 }
 
 
 
 int main() {
-    runProgram();
+    int i = 0;
+    EquipmentList dll;
+    dll.insertFirst("Oscilloscope", "Measurement", "Tektronix TDS210", "OS-12345", true);
+    dll.insertFirst("Function Generator", "Signal Generator", "Agilent 33500B", "FG-67890", false);
+    dll.insertFirst("Multimeter", "Measurement", "Fluke 87V", "MM-24680", true);
+    dll.insertFirst("Power Supply", "Power", "Agilent E3631A", "PS-13579", false);
+    dll.insertFirst("Spectrum Analyzer", "Signal Analyzer", "Rohde & Schwarz FSW", "SA-54321", true);
+    dll.insertFirst("Digital Logic Analyzer", "Measurement", "Keysight MSO-X 3034T", "DL-98765", false);
+    dll.insertFirst("Signal Generator", "Signal Generator", "Keysight 33522A", "SG-11223", true);
+    dll.insertFirst("Function Generator", "Signal Generator", "Rigol DG1022Z", "FG-45678", false);
+    dll.insertFirst("Digital Oscilloscope", "Measurement", "Tektronix MSO2014B", "OS-13579", true);
+    dll.insertFirst("Voltage Regulator", "Power", "LM317", "VR-98765", false);
+    dll.insertFirst("Signal Generator", "Signal Generator", "B&K Precision 4040A", "SG-24680", true);
+    dll.insertFirst("Logic Probe", "Measurement", "HP 1043A", "LP-11223", false);
+    dll.insertFirst("Power Supply", "Power", "Agilent E3646A", "PS-54321", true);
+    dll.insertFirst("Waveform Generator", "Signal Generator", "Teledyne LeCroy ArbStudio 1102", "WG-13579", false);
+    dll.insertFirst("Frequency Counter", "Measurement", "Fluke 725", "FC-98765", true);
+    dll.insertFirst("DC Motor", "Motors", "NEMA 23", "DM-11223", false);
+    dll.insertFirst("Soldering Station", "Tools", "Hakko FX-888D", "SS-24680", true);
+    dll.insertFirst("Logic Analyzer", "Measurement", "Agilent 16802A", "LA-13579", false);
+    dll.insertFirst("Capacitance Meter", "Measurement", "Keysight U1733C", "CM-98765", true);
+    dll.insertFirst("Digital Power Supply", "Power", "Rigol DP832A", "DPS-11223", false);
+
+    while (i == 1) {
+        int Opt;
+        cout << "_____________Lab Equipment Tracker Version 1.0______________" << endl;
+        cout << "Please enter a menu number corresponding to your need." << endl;
+        cout << "1. Lend Equipment" << endl;
+        cout << "2. Add Equipment " << endl;
+        cout << "3. See all Categories " << endl;
+        cout << "4. See all the equipment (sorted) " << endl;
+        cout << "5. Delete an Equipment " << endl;
+        cout << "6. Search Equipment " << endl;
+        cout << "7. Sample Data Display " << endl;
+        cout << "______________________________________________________" << endl;
+
+        cin >> Opt;
+        switch (Opt) {
+        case 1:     //for lend equipments
+            cout << "Lend Equipment option selected" << endl;
+            break;
+        case 2:
+            cout << "Add Equipment option selected" << endl;
+            cout << "_______________________________" << endl;
+            cout << "Please select a category from below list to add a equipment" << endl;
+            dll.printUniqueCategories();
+            cout << "____________________________________" << endl;
+            string selectedCategory = "NULL";
+            cout << "Please enter a category from above list" << endl;
+            cin >> selectedCategory;
+            cout << "Here are all the equipment in the category you selected" << endl;
+            dll.filterByCategory("temp");
+            break;
+        case 7:
+            cout << "Sample data display option selected" << endl;
+            
+            break;
+        default:
+            cout << "Please enter a valid option key" << endl;
+            break;
+        }
+
+    }
+    //runProgram();
+    
+    
+    
     //dummyData();
    /* 
     dll.print();
     */
+
+
+    
+
+   
     return 45;
 }
 
@@ -55,3 +99,6 @@ int main() {
 /*
 
 */
+
+
+//dummy data 
