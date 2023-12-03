ce299c832c894f63e9960bfdcb498a30ccece29e
Sasini2001 <140037735+Sasini2001@users.noreply.github.com>
2023-12-03 21:20:30 +0530

    menu

--- LET.cpp
+++ LET.cpp
@@ -42,32 +42,38 @@ int main() {
         cout << "                    ----- MENU -----" << endl;
         cout << "                                                              " << endl;
 
-        cout << "1. Lend Equipment" << endl;
-        cout << "2. Return Equipment" << endl;
-        cout << "3. Add Equipment by selecting existing category " << endl;
-        cout << "4. See all Categories " << endl;
+        cout << "1. Load data from local machine" << endl;
+        cout << "2. Lend Equipment"<< endl;
+        cout << "3. Return Equipment"<< endl;
+        cout << "4. Add Equipment by selecting existing category " << endl;
         cout << "5. Add Equipment with category" << endl;
         cout << "6. Delete an Equipment " << endl;
         cout << "7. Search Equipment " << endl;
-        cout << "8. Print all the equipments " << endl;
-        cout << "9. Save data locally" << endl;
-        cout << "10. Load data from local machine" << endl;
-        cout << "11. Exit " << RESET_COLOR << endl;
-        cout << "12. Filter by category" << endl;
+        cout << "8. See all Categories " << endl;
+        cout << "9. Filter by category" << endl;
+        cout << "10. Print all the equipments " << endl;
+        cout << "11. Save data locally" << endl;
+        cout << "12. Exit" << RESET_COLOR << endl;
         
         cout << "_____________________________________________________________" << endl;
         cout << "Enter the number corresponding to your instruction : ";
         cin >> Opt;
 
         switch (Opt) {
-        case 1:     //for lend equipments
+        case 1: // Load data from CSV
+            system("cls");
+            cout << "Data loaded from local machine" << endl;
+            dll.loadFromCSV(filename);
+            break;
+
+        case 2:     //for lend equipments
             system("cls");
             cout << "Lend Equipment option selected" << endl;
             cout << "................................................." << endl;
             dll.lendEquipment();
             break;
 
-        case 2:
+        case 3:
             system("cls");
             cout << "Return Equipment option selected" << endl;
             cout << "Enter the serial number of the equipment to be returned: ";
@@ -76,7 +82,7 @@ int main() {
             dll.returnEquipment(serialNumber);
             break;
 
-        case 3:
+        case 4:
             system("cls");
             cout << "Add Equipment option selected" << endl;
             cout << "_______________________________" << endl;
@@ -92,18 +98,12 @@ int main() {
             cout << "                                                              " << endl;
             cout << "Equipment added successfully!" << endl;
             break;
-        case 4:
-            system("cls");
-            // Print all unique categories with numbers or letters
-            dll.printUniqueCategories();
-            break;
         case 5:
             system("cls");
             cout << "Add Equipment with category option is selected" << endl;
             cout << "______________________________________________" << endl;
             dll.addEquipment();
             break;
-
         case 6:
             system("cls");
             cout << "Delete an Equipment option selected" << endl << endl;
@@ -114,7 +114,6 @@ int main() {
             // Delete the equipment by serial number
             dll.deleteEquipmentBySerialNumber(serialNumber);
             break;
-
         case 7:
             system("cls");
             cout << "Enter the equipment serial number : ";
@@ -124,41 +123,42 @@ int main() {
 
         case 8:
             system("cls");
+            // Print all unique categories with numbers or letters
+            dll.printUniqueCategories();
+            break;
+        
+        case 19:
+            system("cls");
+            cout << "Here are the list of unique categories" << endl;
+            dll.printUniqueCategories();
+            cout << "Enter the category you want to filter :" << endl;
+            // cin >> selectedCategory;
+
+             //dll.filterByCategory(selectedCategory);
+            break;
+       
+        
+        case 10:
+            system("cls");
             cout << "Print all the equipments option selected" << endl;
             dll.print();
             break;
-        
-
-        case 9: // Save data to CSV and exit
+       
+       
+        case 11: // Save data to CSV and exit
             system("cls");
             dll.saveToCSV(filename);
             isRunning = false;
             break;
 
-        case 10: // Load data from CSV
-            system("cls");
-            cout << "Data loaded from local machine" << endl;
-            dll.loadFromCSV(filename);
-            break;
-
-        case 11:
-            system("cls");
-            isRunning = false;  // Set the boolean variable to false to exit the loop
-            break;
-
-        case 12: 
-            system("cls");
-            cout << "Here are the list of unique categories" << endl;
-            dll.printUniqueCategories();
-            cout << "Enter the category you want to filter :" << endl;
-           // cin >> selectedCategory;
-
-            //dll.filterByCategory(selectedCategory);
-            break;
-
         default:
             cout << "Please enter a valid option key" << endl;
             isRunning = false;
+            break;
+
+        case 12:
+            system("cls");
+            isRunning = false;  // Set the boolean variable to false to exit the loop
             break;
         }
 
